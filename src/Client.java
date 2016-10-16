c9b5f470654efbfcc0e5cf795c77ff889d234221
Jeffrey Lam <jeffrey.clam95@gmail.com>
2016-10-16 09:11:07 -0700

    Updates to client

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,19 @@
+import logic.MoodAnalysis;
 
 public class Client {
    public static void main(String[] args) {
       System.out.println("Hello, client!");
+      
+      System.out.println("How are you feeling today?");
+      System.out.println("I am feeling ");
+      //Get user input:
+      
+      //Construct Mood
+      
+      //if mood.isNegative()
+         //System.out.println("I'm sorry to hear about that. What's making you feel so down?");
+      //else
+         //System.out.println("I'm happy to hear about that! Is there anything I can do to make the day even better?");
+      //Get user input:
    }
 }
