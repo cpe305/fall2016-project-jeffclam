a1a354a4276584607c97fd641e445a09432dd19a
Jeffrey Lam <jeffrey.clam95@gmail.com>
2016-10-26 15:11:29 -0700

    fixing javaFX maven build problem

---
+++ src/main/java/ui/User.java
@@ -0,0 +1,26 @@
+package ui;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import db.JournalEntry;
+
+public class User {
+  private ArrayList<JournalEntry> journalEntries;
+
+  public User() {
+    journalEntries = new ArrayList<JournalEntry>();
+  }
+
+  public boolean add(JournalEntry entry) {
+    return journalEntries.add(entry);
+  }
+
+  public JournalEntry remove(int index) {
+    return journalEntries.remove(index);
+  }
+
+  public void sort() {
+    Collections.sort(journalEntries);
+  }
+}
